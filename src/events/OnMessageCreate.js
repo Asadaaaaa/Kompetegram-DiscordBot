80b3f081b99878d5df7512dbccc1a1e7826ae02b
Asadaaaaa <asada0schafer@gmail.com>
2022-10-31 16:30:19 +0700

    feat: add prefix for chat command

---
+++ src/events/OnMessageCreate.js
@@ -0,0 +1,29 @@
+import SendGuide from "../commands/SendGuide.js";
+import VerifMenu from "../commands/VerifMenu.js";
+
+class OnMessageCreate {
+  constructor(server, client) {
+    client.on("messageCreate", (e) => {
+      let msgContent =  e.content;
+
+      if(e.author.bot) return;
+      if(!msgContent.startsWith(server.data.config.discord.prefix + " ")) return;
+      
+      let args = msgContent.split(" ");
+
+      switch(args[1]) {
+        case "send_guide": {
+          new SendGuide(server, client, e);
+          break;
+        }
+        
+        case "send_verif_menu": {
+          new VerifMenu(server, client, e);
+          break;
+        }
+      }
+    });
+  }
+};
+
+export default OnMessageCreate;
