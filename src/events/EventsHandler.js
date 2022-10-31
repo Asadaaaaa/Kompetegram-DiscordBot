7481efe4a4b69c47dc0b853a1eea4b3d6c971c8b
Asadaaaaa <asada0schafer@gmail.com>
2022-10-31 16:31:45 +0700

    feat: call class for each events

--- src/events/EventsHandler.js
+++ src/events/EventsHandler.js
@@ -1,12 +1,14 @@
 import OnBotReady from "./OnReady.js";
 import OnGuildMemberAdd from "./OnGuildMemberAdd.js";
+import OnGuildMemberRemove from "./OnGuildMemberRemove.js";
+import OnMessageCreate from "./OnMessageCreate.js";
 
 class EventsHandler {
   constructor(server, client) {
     new OnBotReady(server, client);
     new OnGuildMemberAdd(server, client);
-    
-    return;
+    new OnGuildMemberRemove(server, client);
+    new OnMessageCreate(server, client);
   }
 }
 
